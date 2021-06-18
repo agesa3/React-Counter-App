f3a9ac5d16008658aa3d05a7a709aa45218cb6bb
agesa3 <collinsagesa2000@gmail.com>
2021-06-19 02:46:11 +0300

    more updates

--- src/index.js
+++ src/index.js
@@ -4,37 +4,13 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { createStore } from "redux";
+import allReducers from "./reducers";
+import { Provider } from "react-redux";
 
-//STORE->GLOALIZED STATE
-//ACTION-DESCRIBES WHAT YOU WANT TO DO
-const increment = () => {
-  return {
-    type: "INCREMENT",
-  };
-};
-const decrement = () => {
-  return {
-    type: "DECREMENT",
-  };
-};
-
-//REDUCER
-const counter = (state = 0, action) => {
-  switch (action.type) {
-    case "INCREMENT":
-      return state + 1;
-    case "DECREMENT":
-      return state - 1;
-  }
-};
-
-//store
-let store = createStore(counter);
-
-store.subscribe(() => console.log(store.getState()));
-
-//dispatch
-store.dispatch(increment());
+const store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -42,5 +18,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-
