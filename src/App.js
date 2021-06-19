f471bf091494088799d7a28b760b006f987ecf38
agesa3 <collinsagesa2000@gmail.com>
2021-06-19 03:07:45 +0300

    more updates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { useSelector, useDispatch } from "react-redux";
+import { increment } from "./actions";
+
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const isLogged = useSelector((state) => state.isLogged);
+  const dispatch = useDispatch();
   return (
     <div className="App">
-     
+      <h1>Counter {counter}</h1>
+      <button onClick={() => dispatch(increment())}> + </button>
+      <button>-</button>
+      {isLogged ? <h3>Valuable info I shouldnt see</h3> : ""}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -14,7 +14,9 @@ const store = createStore(
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
