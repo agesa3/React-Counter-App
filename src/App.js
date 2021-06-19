f51f161f34cee360cf8f5026e4b0a343029625a5
agesa3 <collinsagesa2000@gmail.com>
2021-06-19 15:40:14 +0300

    added styling to the counter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useSelector, useDispatch } from "react-redux";
 import { increment, decrement } from "./actions";
-import {Button} from "react-bootstrap";
+import { Button, Container, Navbar } from "react-bootstrap";
 
 function App() {
   const counter = useSelector((state) => state.counter);
@@ -8,15 +8,48 @@ function App() {
   const dispatch = useDispatch();
   return (
     <div className="App">
-      <h1>Counter {counter}</h1>
-      <Button variant="primary" onClick={() => dispatch(increment())}>
-        +
-      </Button>
-      <Button variant="danger" onClick={() => dispatch(decrement())}>
-        -
-      </Button>
-  
-      {isLogged ? <h3>Valuable info I shouldnt see</h3> : ""}
+      <style type="text/css">
+        {`
+    .btn-flat {
+      background-color: purple;
+      color: white;
+    }
+
+    .btn-xxl {
+      padding: 1rem 1.5rem;
+      font-size: 1.5rem;
+      margin:20px;
+      width:100px;
+    }
+    
+    `}
+      </style>
+      <Container>
+        <Navbar expand="lg" variant="light" bg="light">
+          <Navbar.Brand href="#">
+            Simple React Counter Using React Redux
+          </Navbar.Brand>
+        </Navbar>
+        <h1>Counter {counter}</h1>
+
+        <Button
+          variant="primary"
+          size="xxl"
+          onClick={() => dispatch(increment())}
+        >
+          +
+        </Button>
+
+        <Button
+          variant="danger"
+          size="xxl"
+          onClick={() => dispatch(decrement())}
+        >
+          -
+        </Button>
+
+        {isLogged ? <h3>Valuable info I shouldnt see</h3> : ""}
+      </Container>
     </div>
   );
 }
